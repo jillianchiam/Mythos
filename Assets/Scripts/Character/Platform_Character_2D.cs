8b3e42caa8a1f77fae5b91b5d3705dfd94bb5c77
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-01-19 20:26:43 -0800

    Camera + Player
    Camera follows player - Can make non centered while moving
    Player can move horizontally

--- Assets/Scripts/Character/Platform_Character_2D.cs
+++ Assets/Scripts/Character/Platform_Character_2D.cs
@@ -1,16 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System;
 
 public class Platform_Character_2D : MonoBehaviour {
 
+    // Player Movement fields
+    public float speed;
+    public float jumpPower;
+    private float moveHorizontal;
+    private Rigidbody2D rb2d;
+
+    // Animation related fields
+    private Animator animations;
+    private bool doubleJumpEnable;
+    private bool onGround;
+    private bool isIdle;
+
 	// Use this for initialization
 	void Start () {
-		
+        // Attach the rigid body and animations to the player
+        rb2d = GetComponent<Rigidbody2D>();
+        animations = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Translate(-0.05f, 0, 0);
+        RunAnimations();
+        ApplyPlayerMovement();
 	}
+
+    // Change player velocity based on user input
+    void ApplyPlayerMovement()
+    {
+        moveHorizontal = Input.GetAxis("Horizontal");
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
+        SetSpriteDirection();
+        Jump();
+    }
+
+    // Sets direction of player sprite
+    void SetSpriteDirection()
+    {
+        // Face left
+        if (moveHorizontal < -.1)
+            transform.localScale = new Vector3(-1, 1, 1);
+        // Face right
+        else if (moveHorizontal > .1)
+            transform.localScale = new Vector3(1, 1, 1);
+    }
+
+    private void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+            if (onGround || doubleJumpEnable)
+                isIdle = true;
+
+        if (Input.GetButtonUp("Jump"))
+        {
+            isIdle = false;
+            if (onGround)
+            {
+                rb2d.AddForce(Vector2.up * jumpPower);
+                onGround = false;
+                doubleJumpEnable = true;
+            }
+            else if (doubleJumpEnable)
+            {
+                doubleJumpEnable = false;
+                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
+                rb2d.AddForce(Vector2.up * jumpPower);
+            }
+        }
+    }
+
+    // Checks whether player is touching ground or not
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            onGround = true;
+    }
+
+    // Ensures that Animation booleans are associated on each update
+    void RunAnimations()
+    {
+        animations.SetFloat("Movement", Mathf.Abs(moveHorizontal));
+        animations.SetBool("IsIdle", isIdle);
+        animations.SetBool("OnGround", onGround);
+    }
 }
