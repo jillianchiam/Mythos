0a892753d54a21cc11728d00dce0b513e64b1484
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-01-20 18:16:15 -0800

    Character Projectile
    Code to determine current direction
    
    Player can shoot a banana
    
    Other organization

---
+++ Assets/Scripts/Character/Character_Moveset.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character_Moveset : MonoBehaviour {
+    public Rigidbody2D banana;
+    public float bananaSpeed = 10f;
+
+    private Vector2 facingDirection = Vector2.zero;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+            FireBanana();
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 directionVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        // Update the player direction when movement is present
+        if (directionVector != Vector2.zero)
+        {
+            facingDirection = directionVector;
+            facingDirection.Normalize();
+        }
+    }
+
+    void FireBanana()
+    {
+        var bananaInst = Instantiate(banana, transform.position, Quaternion.Euler(new Vector2(0, 0)));
+
+        if (facingDirection.x < 0)
+            bananaInst.velocity = new Vector2(-bananaSpeed, 0);
+        else
+            bananaInst.velocity = new Vector2(bananaSpeed, 0);
+    }
+}
