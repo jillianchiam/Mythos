eb747c07564cc094e7f35dad1060e773a71f776a
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-01-20 16:08:49 -0800

    Jumping
    Jumping is in its own script file
    Walljump is broken

--- Assets/Scripts/Character/Platform_Character_2D.cs
+++ Assets/Scripts/Character/Platform_Character_2D.cs
@@ -9,16 +9,11 @@ public class Platform_Character_2D : MonoBehaviour {
 
     // Player Movement fields
     public float speed;
-    public float jumpPower;
     private float moveHorizontal;
     private Rigidbody2D rb2d;
 
     // Animation related fields
     private Animator animations;
-    private bool doubleJumpEnable;
-    private bool onGround;
-    private bool isJumping;
-    private bool onVerticalSurface;
 
     // Use this for initialization
     void Start () {
@@ -39,7 +34,6 @@ public class Platform_Character_2D : MonoBehaviour {
         moveHorizontal = Input.GetAxis("Horizontal");
         rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
         SetSpriteDirection();
-        Jump();
     }
 
     // Sets direction of player sprite
@@ -52,47 +46,10 @@ public class Platform_Character_2D : MonoBehaviour {
         else if (moveHorizontal > .1)
             transform.localScale = new Vector3(1, 1, 1);
     }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump"))
-            if (onGround || doubleJumpEnable)
-            {
-                isJumping = true;
-            }
-
-        if (Input.GetButtonUp("Jump"))
-        {
-            isJumping = false;
-            if (onGround)
-            {
-                rb2d.AddForce(Vector2.up * jumpPower);
-                onGround = false;
-                doubleJumpEnable = true;
-            }
-            else if (doubleJumpEnable)
-            {
-                doubleJumpEnable = false;
-                rb2d.velocity = new Vector2(rb2d.velocity.x, 0);
-                rb2d.AddForce(Vector2.up * jumpPower);
-            }
-        }
-    }
-    
-    // Checks whether player is touching ground or not
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.CompareTag("Ground"))
-            onGround = true;
-        else if (collision.gameObject.CompareTag("Solid_Wall"))
-            onVerticalSurface = true;
-    }
     
     // Ensures that Animation booleans are associated on each update
     void RunAnimations()
     {
         animations.SetFloat("Movement", Mathf.Abs(moveHorizontal));
-        animations.SetBool("IsJumping", isJumping);
-        animations.SetBool("OnGround", onGround);
     }
 }
