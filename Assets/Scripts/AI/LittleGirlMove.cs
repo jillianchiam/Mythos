b7571dac729df0313bfb867bbfcef09bfe9092d0
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-02-06 19:42:17 -0800

    Added some pics to scene

---
+++ Assets/Scripts/AI/LittleGirlMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LittleGirlMove : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var player = GameObject.Find("Player").transform;
+    }
+}
