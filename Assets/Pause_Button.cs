acb75d1cb62edc0205f7316f6c9baf7ae8e344a6
jillianchiam <39228399+jillianchiam@users.noreply.github.com>
2019-03-20 19:35:42 -0700

    Insert Pause and Resume Button
    Can only br clicked with mouse though

---
+++ Assets/Pause_Button.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause_Button : MonoBehaviour {
+
+    public GameObject Pausemenu, PauseButton;
+
+    // Use this for initialization
+    public void Pause()
+    {
+        Pausemenu.SetActive(true);
+        PauseButton.SetActive(false);
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    public void Resume()
+    {
+        Pausemenu.SetActive(false);
+        PauseButton.SetActive(true);
+        Time.timeScale = 1;
+    }
+}
