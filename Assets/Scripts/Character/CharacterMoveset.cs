4c1bde4a6d87e4c7fbcf757e7b693e03bedf4451
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-03-20 17:44:48 -0700

    Applied proper charge for projectile

--- Assets/Scripts/Character/CharacterMoveset.cs
+++ Assets/Scripts/Character/CharacterMoveset.cs
@@ -9,10 +9,26 @@ public class CharacterMoveset : MonoBehaviour
     [SerializeField] private float cooldown = 0.5f;
 
     private bool mainProjRefreshing = false;
+    private float chargeCount = 0.0f;
+    private float chargeTime = 2.0f;
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButton("Fire1") && chargeCount < chargeTime)               // Count time of button held down
+        {
+            chargeCount = chargeCount + 1 * Time.deltaTime;                     // Use delta time to properly count seconds
+        }
+        else if (chargeCount >= chargeTime && Input.GetButtonUp("Fire1"))       // Charge time must be met and button must be released to fire                                                                         // After specified charge time fire the projectile
+        {
+            FireProjectile();                                                   // Unleash projectile
+            chargeCount = 0;                                                    // Reset charge counter
+        }
+        else if (Input.GetButtonUp("Fire1"))                                    // Button released but not charged enough
+        {
+            chargeCount = 0;                                                    // Reset charge count
+        }
+        /*
         if (Input.GetButtonDown("Fire1"))                                                                                   // If key is pressed execute code to fire a projectile
         {
             if (!mainProjRefreshing)
@@ -22,6 +38,7 @@ public class CharacterMoveset : MonoBehaviour
                 Invoke("MainProjectileRefreshed", cooldown);                                                                // Activate the cooldown so that another can be fired later
             }
         }
+        */
     }
 
     void FireProjectile()
