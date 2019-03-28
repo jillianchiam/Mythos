706749fb49b369c3c12f6b0cad9c2111c59c3e75
tmann123 <43198653+tmann123@users.noreply.github.com>
2019-03-28 03:41:29 -0700

    game menu

---
+++ Assets/Scripts/UI/PauseButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseButton : MonoBehaviour {
+
+    public GameObject pauseMenu;
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Resume()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/Scripts/UI/PauseScreen.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Cancel"))
+            GameObject.Find("Canvas").GetComponent<PauseButton>().Pause();
+            //GameObject.Find("Canvas").transform.GetChild(0).gameObject.SetActive(true);
+
+    }
+}
